4daccb9fe66a20964c5abb900a741d036aa9cc5d
Infi <infistarx3@gmail.com>
2024-01-28 20:26:26 -0300

    notas update

---
+++ notas/ARM.php
@@ -0,0 +1,18 @@
+<?php require('../layouts/PHP/head-1.php') ?>
+    <title>Inby ❤ - ARM</title>
+<?php require('../layouts/PHP/head-2.php') ?>
+
+<?php require('../layouts/PHP/crear_archivo_content.php') ?>
+
+	<?php require('../layouts/PHP/header-word-archive.php') ?>
+
+    <section class='wordarchive detector'>
+        <h1>Tulita</h1>
+        <p>holaholahola</p>
+        <img src="../../notas/images/64c7b08d54694_IMG_20230727_091311_948.jpg">
+    </section>
+
+    <?php require('../layouts/PHP/scripts.php') ?>
+</body>
+</html>
+            
---
+++ notas/Existencialismo y Fundamentos de la Moral.php
@@ -0,0 +1,69 @@
+<?php require('../layouts/PHP/head-1.php') ?>
+    <title>Inby ❤ - Existencialismo y Fundamentos de la Moral </title>
+<?php require('../layouts/PHP/head-2.php') ?>
+
+<?php require('../layouts/PHP/crear_archivo_content.php') ?>
+
+    <?php require('../layouts/PHP/background_default.php') ?>
+	<?php require('../layouts/PHP/new-header-word-archive.php') ?>
+
+    <section class='wordarchive detector'>
+        <h1>Existencialismo y Fundamentos de la Moral</h1>
+        <h3 class='h3__autor'>Autor: Infisito</h3>
+        <p id='markdownParagraph'>#Existencialismo#<br />
+<br />
+_"Soy libre con un grado de responsabilidad"_, se comienza a ver esta filosofía de una manera pesimista; _"Uno también se hace responsable de las acciones de los demás"._ Porque toda acción sirve como ejemplo para los demás.<br />
+<br />
+**La gente puede intencionar a los demás a hacer alguna acción**<br />
+Ya que si muchas personas hacen una misma acción, la norma moral cambia, _la sociedad cambia._<br />
+<br />
+**La responsabilidad y libertad:** La primera consecuencia de nuestra libertad es que, al actuar de manera libre y tomando decisiones que provienen de nuestro propio interior, asumimos la responsabilidad de esas acciones. A veces, esta responsabilidad puede sentirse como un peso o una carga.<br />
+<br />
+<br />
+##Cosas##<br />
+- Ya tienen una definición dada.<br /><br />
+- Esencia.<br /><br />
+- Después de la esencia tenemos la existencia.<br /><br />
+<br />
+**Esencia:** Definición de lo que algo es o era. En el ser humano esa esencia solo se alcanza al morir, ya que viviendo siempre hay posibilidad de cambio; "una mala acción solo es un momento, no la vida entera".<br />
+<br />
+**Cosificar:** Tratar a una persona como una cosa.<br />
+quitar la libertad de alguien al darle una definición.<br />
+ejemplo: Una relación amorosa basada únicamente en la apariencia física. Se tratan como un objeto para satisfacer sus deseos físicos o estéticos, sin tener en cuenta su personalidad, intereses o emociones.<br />
+<br />
+##Seres humanos##<br />
+- Primero el ser humano existe y luego tiene su esencia<br />
+ya que, un ser humano en la filosofía del existencialismo, no lo pueden definir como algo o alguien, ya que ahí le estarían dando una esencia y eso sería quitarle su libertad y autenticidad.<br /><br />
+- Todos los seres humanos nacen libres, ya que nadie los define ni los limita.<br />
+<br />
+Somos responsables de nuestras acciones y sus consecuencias. Nuestras acciones pueden influir en los demás y cambiar las normas sociales. Por lo tanto, es importante ser conscientes de cómo nuestras elecciones afectan a los demás y al mundo.<br />
+Ejemplo: "una persona no saluda", la buena acción es saludar, si esta persona no saluda otro puede no saludar también, entonces, "no saludar" se comienza a ver como una buena acción. Una acción puede replicarse y cambiar la norma.<br />
+<br />
+<br />
+##Representantes del existencialismo:##<br />
+- _Jean Paul Sartre_<br />
+- _Simone de Beauvoir_<br />
+<br />
+##Los fundamentos de la moral##<br />
+<br />
+**La naturaleza humana;** Todos se deben comportar según corresponda a esto. Todas las normas se cumplen por igual.<br />
+<br />
+**El deber;** Con nuestras acciones, creamos normas morales y éticas. Cuando consideramos una norma importante y valiosa, sentimos el deber de cumplirla porque está en línea con nuestros valores personales. <br />
+<br />
+**El amor;** Amor al prójimo y a ti mismo. "no hagas al otro lo que no te gustaría que te hagan a ti". Es para que se respeten y para que exista una unión. Según la frase es como "respeta para que el Karma no te llegue".<br />
+Cuando hay una persona sin amor propio está _**ahí**_ para romper las normas porque no _**me**_ importa lo que me pase.<br />
+<br />
+**El contrato social;** "Yo le doy algo a la sociedad para que la sociedad también me aporte". Es como un "dar para tener", la sociedad da unos beneficios y uno tiene que comportarse bien por esas ventajas.<br />
+<br />
+**la utilidad;** Este principio se basa en hacer cosas para tener beneficios a cambio de un buen comportamiento.<br />
+"Tu quieres a alguien que te pueda hacer feliz". John Stuart Mill<br />
+Todas las relaciones se forman por algún motivo, como un intercambio o para satisfacer necesidades.<br />
+<br />
+Cuando falta uno de estos fundamentos, la norma se debilita y puede desaparecer con el tiempo. Pero si sigue manteniendo todos estos, entonces permanecerá vigente a lo largo del tiempo.</p>
+        <img src="../../notas/images/64d1b0b5abf6e_e4cc03fa37be34c26911c0dbf780fd67.jpg"> 
+    </section>
+
+    <?php require('../layouts/PHP/scripts.php') ?>
+</body>
+</html>
+            
