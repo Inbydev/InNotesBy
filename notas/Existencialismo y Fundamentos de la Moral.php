fe13f7c94b78e923d8ac68a2c1aac009927ef681
Infi <infistarx3@gmail.com>
2024-02-06 18:05:39 -0300

    new note

---
+++ notas/Existencialismo y Fundamentos de la Moral.php
@@ -0,0 +1,97 @@
+<?php
+require('../layouts/PHP/head.php');
+$title = 'InNotesBy ❤ - Existencialismo y Fundamentos de la Moral';
+$description = 'Proyecto de Inbydev para crear Notas!';
+$othercss = '<link rel="stylesheet" href="/layouts/CSS/Notes.css">';
+head($title, $description, $othercss);
+?>
+    <?php require('../layouts/PHP/header-notes.php') ?>
+
+    <section class='wordarchive'>
+        <h1>Existencialismo y Fundamentos de la Moral</h1>
+        <h2 class='autor'>Autor: Infisito</h2>
+        <p class='markdown__container'>
+## Existencialismo
+
+***Soy libre con un grado de responsabilidad***, se comienza a ver esta filosofía de una manera pesimista; ***Uno también se hace responsable de las acciones de los demás***. Porque toda acción sirve como ejemplo para los demás.
+
+
+### **La gente puede hacer que los demás hagan alguna acción**
+
+Ya que si muchas personas hacen una misma acción, *la norma moral cambia, la sociedad cambia.*
+
+
+**La responsabilidad y libertad:** La primera consecuencia de nuestra libertad es que, al actuar de manera libre y tomando decisiones que provienen de nuestro propio interior, asumimos la responsabilidad de esas acciones. A veces, esta responsabilidad puede sentirse como un peso o una carga.
+
+
+Cosas
+
+- Ya tienen una definición dada.
+
+- Esencia.
+
+- Después de la esencia tenemos la existencia.
+
+
+**Esencia:** Definición de lo que algo es o era. En el ser humano esa esencia solo se alcanza al morir, ya que viviendo siempre hay posibilidad de cambio; &quot;una mala acción solo es un momento, no la vida entera&quot;.
+
+
+**Cosificar:** Tratar a una persona como una cosa.
+
+quitar la libertad de alguien al darle una definición.
+
+ejemplo: Una relación amorosa basada únicamente en la apariencia física. Se tratan como un objeto para satisfacer sus deseos físicos o estéticos, sin tener en cuenta su personalidad, intereses o emociones.
+
+
+### Seres humanos
+
+- Primero el ser humano existe y luego tiene su esencia
+
+ya que, un ser humano en la filosofía del existencialismo, no lo pueden definir como algo o alguien, ya que ahí le estarían dando una esencia y eso sería quitarle su libertad y autenticidad.
+
+- Todos los seres humanos nacen libres, ya que nadie los define ni los limita.
+
+
+Somos responsables de nuestras acciones y sus consecuencias. Nuestras acciones pueden influir en los demás y cambiar las normas sociales. Por lo tanto, es importante ser conscientes de cómo nuestras elecciones afectan a los demás y al mundo.
+
+Ejemplo: &quot;una persona no saluda&quot;, la buena acción es saludar, si esta persona no saluda otro puede no saludar también, entonces, &quot;no saludar&quot; se comienza a ver como una buena acción. Una acción puede replicarse y cambiar la norma.
+
+
+### Representantes del existencialismo:
+
+- *Jean Paul Sartre*
+
+- *Simone de Beauvoir*
+
+
+### Los fundamentos de la moral
+
+**La naturaleza humana;** Todos se deben comportar según corresponda a esto. Todas las normas se cumplen por igual.
+
+**El deber;** Con nuestras acciones, creamos normas morales y éticas. Cuando consideramos una norma importante y valiosa, sentimos el deber de cumplirla porque está en línea con nuestros valores personales.
+
+
+**El amor;** Amor al prójimo y a ti mismo. &quot;no hagas al otro lo que no te gustaría que te hagan a ti&quot;. Es para que se respeten y para que exista una unión. Según la frase es como &quot;respeta para que el Karma no te llegue&quot;.
+
+Cuando hay una persona sin amor propio está ***ahí*** para romper las normas porque no ***me*** importa lo que me pase.
+
+
+
+**El contrato social;** &quot;Yo le doy algo a la sociedad para que la sociedad también me aporte&quot;. Es como un &quot;dar para tener&quot;, la sociedad da unos beneficios y uno tiene que comportarse bien por esas ventajas.
+
+
+**la utilidad;** Este principio se basa en hacer cosas para tener beneficios a cambio de un buen comportamiento.
+
+&quot;Tu quieres a alguien que te pueda hacer feliz&quot;. John Stuart Mill
+
+Todas las relaciones se forman por algún motivo, como un intercambio o para satisfacer necesidades.
+
+
+Cuando falta uno de estos fundamentos, la norma se debilita y puede desaparecer con el tiempo. Pero si sigue manteniendo todos estos, entonces permanecerá vigente a lo largo del tiempo.
+</p>
+        <img src="">
+    </section>
+
+    <?php require('../layouts/PHP/scripts.php') ?>
+</body>
+</html>
