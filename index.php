a9acb55c0e037a4e1d0c9e1fe8b55731a30de5df
Inbydev <139036190+Inbydev@users.noreply.github.com>
2024-03-11 23:11:40 -0300

    Changed the license, update the footer for it

--- index.php
+++ index.php
@@ -59,14 +59,14 @@ head($title, $description, $headerjs, $otherjs, $othercss);
         </section>
 
         <footer>
-            <p>
-                <a href="https://github.com/Inbydev/Inby.dev">InNotesBy</a> © 2023 - 2024
-                by
-                <a href="https://github.com/Inbydev">Inbydev</a>
-                is licensed under
-                <a href="http://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" style="display:inline-block;">CC BY-SA 4.0
-                </a>
-            </p>
+		<p>
+			<a href="https://github.com/Inbydev/Inby.dev">InNotesBy</a> © 2023 - 2024
+			by
+			<a href="https://github.com/Inbydev">Inbydev</a>
+			is licensed under
+			<a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" style="display:inline-block;">CC BY 4.0
+			</a>
+		</p>
         </footer>
     </main>
     
