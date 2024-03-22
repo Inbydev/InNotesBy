296418c9591b2b3496f4d5dd94e8f1a08a936df5
Infi <infistarx3@gmail.com>
2024-03-22 17:47:19 -0300

    new Note

---
+++ notas/Ola.php
@@ -0,0 +1,21 @@
+<?php
+require('../layouts/PHP/head.php');
+$title = 'InNotesBy ❤ - Ola';
+$description = 'Proyecto de Inbydev para crear Notas!';
+$othercss = '<link rel="stylesheet" href="/InNotesBy/layouts/CSS/Notes.css">';
+head($title, $description, $othercss);
+?>
+    <?php require('../layouts/PHP/header-notes.php') ?>
+
+    <section class='wordarchive'>
+        <h1>Ola</h1>
+        <h2 class='autor'>Autor: Jijiji</h2>
+        <p class='markdown__container'>
+Solo vengo a probar como funciona esto ✨
+</p>
+        <img src="">
+    </section>
+
+    <?php require('../layouts/PHP/scripts.php') ?>
+</body>
+</html>
