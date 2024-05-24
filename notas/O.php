44ba42ef855c739cc8f65e6f4305ee1bd0d3c2d2
Inbydev <owner@inby.dev>
2024-05-24 18:46:19 -0400

    new note

---
+++ notas/O.php
@@ -0,0 +1,27 @@
+<?php
+require('../layouts/PHP/head.php');
+$title = 'InNotesBy ❤ - O';
+$description = 'Proyecto de Inbydev para crear Notas!';
+$othercss = '<link rel="stylesheet" href="/InNotesBy/layouts/CSS/Notes.css">';
+head($title, $description, $othercss);
+?>
+    <?php require('../layouts/PHP/header-notes.php') ?>
+
+    <section class='wordarchive'>
+        <h1>O</h1>
+        <h2 class='autor'>Autor:  O</h2>
+        <p class='markdown__container'>
+## oli
+
+me enoje nestgoy muy en ojasdo quiero pegarl,ea, a kla fgebnte porquewhue estgouj muy senaopjado !!!!!!
+
+o
+
+[roblo camilo](https://www.roblox.com/users/5804572705/profile)
+</p>
+        <img src="">
+    </section>
+
+    <?php require('../layouts/PHP/scripts.php') ?>
+</body>
+</html>
