5e66f9c991880f31a2c06bac59ccd8e869cbeec2
Inbydev <owner@inby.dev>
2024-05-25 01:04:09 -0400

    Add TO DO: comment for later

--- layouts/JS/exclude__characters.js
+++ layouts/JS/exclude__characters.js
@@ -3,6 +3,7 @@ function validarCaracteresEspeciales() {
 
     var exclude = /^[^\/\\\*\?"<>|]+$/;
 
+    // TO DO: change alert to an notification or popup
     if (!exclude.test(nombreArchivo)) {
         alert("El nombre de la nota '" + nombreArchivo + "' contiene caracteres no permitidos. Por favor, elige otro nombre.");
         return false;
