cb87087fe45fce41ea02fc80849210585525e1e0
Inbydev <owner@inby.dev>
2024-05-25 00:57:29 -0400

    deleted header__mobile + deleted ionicon library + copy link on header-notes

--- layouts/JS/header__mobile.js
+++
@@ -1,6 +0,0 @@
-let header__mobile__toggle = document.querySelector('.header__mobile__toggle');
-let header__mobile = document.querySelector('.header__mobile');
-
-header__mobile__toggle.onclick = function(){
-	header__mobile.classList.toggle('active');
-}
