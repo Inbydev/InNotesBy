ec7f5ca9de106de2d6a496e675dc920712bc3ad1
Inbydev <owner@inby.dev>
2024-05-25 17:41:30 -0400

    Added TODO to this file

--- layouts/PHP/find_notes.php
+++ layouts/PHP/find_notes.php
@@ -17,6 +17,7 @@ HTML;
     foreach ($archivos as $archivo) {
         $nombreArchivo = basename($archivo, '.php');
         
+        // TODO: Modify the style of the find_notes function
         echo "  <a class='nota' href=\"$archivo\" oncontextmenu=\"showContextMenu(event, '$archivo'); return false;\">
                     <svg viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg' fill='#FFFFFF'>
                     <g id='SVGRepo_bgCarrier' stroke-width='0'></g><g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g><g id='SVGRepo_iconCarrier'><path d='M6.345 5h2.1v6.533H6.993l.055-5.31-1.774 5.31H4.072l-1.805-5.31c.04.644.06 5.31.06 5.31H1V5h2.156s1.528 4.493 1.577 4.807L6.345 5zm6.71 3.617v-3.5H11.11v3.5H9.166l2.917 2.916L15 8.617h-1.945z'></path></g></svg>
